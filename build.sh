31bc6460e3dd71faa78846e289fca3849f696205
Koushik Dutta <koushd@gmail.com>
2014-08-23 23:09:09 -0700

    unbrand, add background adb server

---
+++ build.sh
@@ -0,0 +1,1 @@
+cat ../CastReceiver/jsencrypt.js ../CastReceiver/util.js ../CastReceiver/socket.js ../CastReceiver/httpserver.js ../CastReceiver/adb.js ../CastReceiver/adbmain.js | uglifyjs > src/background.js
